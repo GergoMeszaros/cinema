8cebea9dc1a16a9f98a6fcbd89b35a503cb88983
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2025-03-19 00:57:49 +0100

    Update routes with naming

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/movies', [MovieController::class, 'showAll']);
-Route::get('/movies/{id}', [MovieController::class, 'show']);
-Route::post('/movies/new', [MovieController::class, 'create']);
-Route::patch('/movies/{id}', [MovieController::class, 'edit']);
-Route::delete('/movies/{id}', [MovieController::class, 'remove']);
+Route::get('/movies', [MovieController::class, 'showAll'])->name('movies.showAll');
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::post('/movies/new', [MovieController::class, 'create'])->name('movies.create');
+Route::patch('/movies/{id}', [MovieController::class, 'edit'])->name('movies.edit');
+Route::delete('/movies/{id}', [MovieController::class, 'remove'])->name('movies.remove');
 
-Route::post('/showtime_details/new', [ShowtimeDetailsController::class, 'create']);
-Route::delete('/showtime_details/{id}', [ShowtimeDetailsController::class, 'remove']);
-Route::patch('/showtime_details/{id}', [ShowtimeDetailsController::class, 'edit']);
+Route::post('/showtime_details/new', [ShowtimeDetailsController::class, 'create'])->name('showtime_details.new');
+Route::delete('/showtime_details/{id}', [ShowtimeDetailsController::class, 'remove'])->name('showtime_details.remove');
+Route::patch('/showtime_details/{id}', [ShowtimeDetailsController::class, 'edit'])->name('showtime_details.edit');
 
-Route::post('/cover_picture/new', [CoverPictureController::class, 'create']);
-Route::delete('/cover_picture/{id}', [CoverPictureController::class, 'remove']);
-Route::patch('/cover_picture/{id}', [CoverPictureController::class, 'edit']);
+Route::post('/cover_picture/new', [CoverPictureController::class, 'create'])->name('cover_picture.new');
+Route::delete('/cover_picture/{id}', [CoverPictureController::class, 'remove'])->name('cover_picture.remove');
+Route::patch('/cover_picture/{id}', [CoverPictureController::class, 'edit'])->name('cover_picture.edit');
